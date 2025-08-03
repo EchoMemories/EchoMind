c33758e0a20d18c05600d70bcd6e81c67547c712
0x101008 <shtnb@outlook.com>
2025-08-03 14:53:17 +0800

    test

--- v0.1/TestCode/Ollama_api_test.py
+++ v0.1/TestCode/Ollama_api_test.py
@@ -1,11 +1,24 @@
 from ollama import chat
-
+think_switch , userinput= int(input('是否开启思维链(是输入1，否输入0)：')) , input('用户输入：')
 messages = [
   {
     'role': 'user',
-    'content': '你好',
+    'content': userinput,
   },
 ]
+response = chat('deepseek-r1:7b', messages=messages, stream=True)
 
-response = chat('deepseek-r1:7b', messages=messages)
-print(response['message']['content'])
+think_count = 0
+def stream_print(response):
+  global think_count
+  global think_switch
+  for messages in response:
+    if think_switch==0:
+      if think_count == 2:
+        print(messages['message']['content'], end='', flush=True)
+      if messages['message']['content'] == '<think>' or messages['message']['content'] == '</think>':
+        think_count += 1
+    else:
+      print(messages['message']['content'], end='', flush=True)
+
+stream_print(response=response)
